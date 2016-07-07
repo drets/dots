dd7945f849e887fabbbb7d93f5b05a1cc428975c
Dmytro Rets <dmitryrets@gmail.com>
2016-07-08 01:35:25 +0200

    Switch between 2 last views: Escape -> s

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -221,7 +221,7 @@ root.buttons(awful.util.table.join(
 globalkeys = awful.util.table.join(
     awful.key({ modkey,           }, "Left",   awful.tag.viewprev       ),
     awful.key({ modkey,           }, "Right",  awful.tag.viewnext       ),
-    awful.key({ modkey,           }, "Escape", awful.tag.history.restore),
+    awful.key({ modkey,           }, "s", awful.tag.history.restore),
 
     awful.key({ modkey,           }, "j",
         function ()
