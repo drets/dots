e2a04ff049968572b05773e5e7b2f1f2c5474461
Dmytro Rets <dmitryrets@gmail.com>
2016-09-25 22:39:21 +0200

    Hardcode awesome theme pathes.

--- .config/awesome/themes/default/theme.lua
+++ .config/awesome/themes/default/theme.lua
@@ -34,13 +34,13 @@ theme.border_marked = "#91231c"
 --theme.taglist_bg_focus = "#ff0000"
 
 -- Display the taglist squares
-theme.taglist_squares_sel   = "/nix/store/q59yrc01mafcsmdqk02dsf4523702fhk-awesome-3.5.9/share/awesome/themes/default/taglist/squarefw.png"
-theme.taglist_squares_unsel = "/nix/store/q59yrc01mafcsmdqk02dsf4523702fhk-awesome-3.5.9/share/awesome/themes/default/taglist/squarew.png"
+theme.taglist_squares_sel   = "/home/drets/src/dots/.config/awesome/themes/default/taglist/squarefw.png"
+theme.taglist_squares_unsel = "/home/drets/src/dots/.config/awesome/themes/default/taglist/squarew.png"
 
 -- Variables set for theming the menu:
 -- menu_[bg|fg]_[normal|focus]
 -- menu_[border_color|border_width]
-theme.menu_submenu_icon = "/nix/store/q59yrc01mafcsmdqk02dsf4523702fhk-awesome-3.5.9/share/awesome/themes/default/submenu.png"
+theme.menu_submenu_icon = "/home/drets/src/dots/.config/awesome/themes/default/submenu.png"
 theme.menu_height = 15
 theme.menu_width  = 100
 
@@ -50,50 +50,47 @@ theme.menu_width  = 100
 --theme.bg_widget = "#cc0000"
 
 -- Define the image to load
-theme.titlebar_close_button_normal = "/nix/store/q59yrc01mafcsmdqk02dsf4523702fhk-awesome-3.5.9/share/awesome/themes/default/titlebar/close_normal.png"
-theme.titlebar_close_button_focus  = "/nix/store/q59yrc01mafcsmdqk02dsf4523702fhk-awesome-3.5.9/share/awesome/themes/default/titlebar/close_focus.png"
+theme.titlebar_close_button_normal = "/home/drets/src/dots/.config/awesome/themes/default/titlebar/close_normal.png"
+theme.titlebar_close_button_focus  = "/home/drets/src/dots/.config/awesome/themes/default/titlebar/close_focus.png"
 
-theme.titlebar_ontop_button_normal_inactive = "/nix/store/q59yrc01mafcsmdqk02dsf4523702fhk-awesome-3.5.9/share/awesome/themes/default/titlebar/ontop_normal_inactive.png"
-theme.titlebar_ontop_button_focus_inactive  = "/nix/store/q59yrc01mafcsmdqk02dsf4523702fhk-awesome-3.5.9/share/awesome/themes/default/titlebar/ontop_focus_inactive.png"
-theme.titlebar_ontop_button_normal_active = "/nix/store/q59yrc01mafcsmdqk02dsf4523702fhk-awesome-3.5.9/share/awesome/themes/default/titlebar/ontop_normal_active.png"
-theme.titlebar_ontop_button_focus_active  = "/nix/store/q59yrc01mafcsmdqk02dsf4523702fhk-awesome-3.5.9/share/awesome/themes/default/titlebar/ontop_focus_active.png"
+theme.titlebar_ontop_button_normal_inactive = "/home/drets/src/dots/.config/awesome/themes/default/titlebar/ontop_normal_inactive.png"
+theme.titlebar_ontop_button_focus_inactive  = "/home/drets/src/dots/.config/awesome/themes/default/titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_active = "/home/drets/src/dots/.config/awesome/themes/default/titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_active  = "/home/drets/src/dots/.config/awesome/themes/default/titlebar/ontop_focus_active.png"
 
-theme.titlebar_sticky_button_normal_inactive = "/nix/store/q59yrc01mafcsmdqk02dsf4523702fhk-awesome-3.5.9/share/awesome/themes/default/titlebar/sticky_normal_inactive.png"
-theme.titlebar_sticky_button_focus_inactive  = "/nix/store/q59yrc01mafcsmdqk02dsf4523702fhk-awesome-3.5.9/share/awesome/themes/default/titlebar/sticky_focus_inactive.png"
-theme.titlebar_sticky_button_normal_active = "/nix/store/q59yrc01mafcsmdqk02dsf4523702fhk-awesome-3.5.9/share/awesome/themes/default/titlebar/sticky_normal_active.png"
-theme.titlebar_sticky_button_focus_active  = "/nix/store/q59yrc01mafcsmdqk02dsf4523702fhk-awesome-3.5.9/share/awesome/themes/default/titlebar/sticky_focus_active.png"
+theme.titlebar_sticky_button_normal_inactive = "/home/drets/src/dots/.config/awesome/themes/default/titlebar/sticky_normal_inactive.png"
+theme.titlebar_sticky_button_focus_inactive  = "/home/drets/src/dots/.config/awesome/themes/default/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_active = "/home/drets/src/dots/.config/awesome/themes/default/titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_active  = "/home/drets/src/dots/.config/awesome/themes/default/titlebar/sticky_focus_active.png"
 
-theme.titlebar_floating_button_normal_inactive = "/nix/store/q59yrc01mafcsmdqk02dsf4523702fhk-awesome-3.5.9/share/awesome/themes/default/titlebar/floating_normal_inactive.png"
-theme.titlebar_floating_button_focus_inactive  = "/nix/store/q59yrc01mafcsmdqk02dsf4523702fhk-awesome-3.5.9/share/awesome/themes/default/titlebar/floating_focus_inactive.png"
-theme.titlebar_floating_button_normal_active = "/nix/store/q59yrc01mafcsmdqk02dsf4523702fhk-awesome-3.5.9/share/awesome/themes/default/titlebar/floating_normal_active.png"
-theme.titlebar_floating_button_focus_active  = "/nix/store/q59yrc01mafcsmdqk02dsf4523702fhk-awesome-3.5.9/share/awesome/themes/default/titlebar/floating_focus_active.png"
+theme.titlebar_floating_button_normal_inactive = "/home/drets/src/dots/.config/awesome/themes/default/titlebar/floating_normal_inactive.png"
+theme.titlebar_floating_button_focus_inactive  = "/home/drets/src/dots/.config/awesome/themes/default/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_active = "/home/drets/src/dots/.config/awesome/themes/default/titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_active  = "/home/drets/src/dots/.config/awesome/themes/default/titlebar/floating_focus_active.png"
 
-theme.titlebar_maximized_button_normal_inactive = "/nix/store/q59yrc01mafcsmdqk02dsf4523702fhk-awesome-3.5.9/share/awesome/themes/default/titlebar/maximized_normal_inactive.png"
-theme.titlebar_maximized_button_focus_inactive  = "/nix/store/q59yrc01mafcsmdqk02dsf4523702fhk-awesome-3.5.9/share/awesome/themes/default/titlebar/maximized_focus_inactive.png"
-theme.titlebar_maximized_button_normal_active = "/nix/store/q59yrc01mafcsmdqk02dsf4523702fhk-awesome-3.5.9/share/awesome/themes/default/titlebar/maximized_normal_active.png"
-theme.titlebar_maximized_button_focus_active  = "/nix/store/q59yrc01mafcsmdqk02dsf4523702fhk-awesome-3.5.9/share/awesome/themes/default/titlebar/maximized_focus_active.png"
+theme.titlebar_maximized_button_normal_inactive = "/home/drets/src/dots/.config/awesome/themes/default/titlebar/maximized_normal_inactive.png"
+theme.titlebar_maximized_button_focus_inactive  = "/home/drets/src/dots/.config/awesome/themes/default/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_active = "/home/drets/src/dots/.config/awesome/themes/default/titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_active  = "/home/drets/src/dots/.config/awesome/themes/default/titlebar/maximized_focus_active.png"
 
-theme.wallpaper = "/nix/store/q59yrc01mafcsmdqk02dsf4523702fhk-awesome-3.5.9/share/awesome/themes/default/background.png"
+theme.wallpaper = "/home/drets/src/dots/.config/awesome/themes/default/background.png"
 
 -- You can use your own layout icons like this:
-theme.layout_fairh = "/nix/store/q59yrc01mafcsmdqk02dsf4523702fhk-awesome-3.5.9/share/awesome/themes/default/layouts/fairhw.png"
-theme.layout_fairv = "/nix/store/q59yrc01mafcsmdqk02dsf4523702fhk-awesome-3.5.9/share/awesome/themes/default/layouts/fairvw.png"
-theme.layout_floating  = "/nix/store/q59yrc01mafcsmdqk02dsf4523702fhk-awesome-3.5.9/share/awesome/themes/default/layouts/floatingw.png"
-theme.layout_magnifier = "/nix/store/q59yrc01mafcsmdqk02dsf4523702fhk-awesome-3.5.9/share/awesome/themes/default/layouts/magnifierw.png"
-theme.layout_max = "/nix/store/q59yrc01mafcsmdqk02dsf4523702fhk-awesome-3.5.9/share/awesome/themes/default/layouts/maxw.png"
-theme.layout_fullscreen = "/nix/store/q59yrc01mafcsmdqk02dsf4523702fhk-awesome-3.5.9/share/awesome/themes/default/layouts/fullscreenw.png"
-theme.layout_tilebottom = "/nix/store/q59yrc01mafcsmdqk02dsf4523702fhk-awesome-3.5.9/share/awesome/themes/default/layouts/tilebottomw.png"
-theme.layout_tileleft   = "/nix/store/q59yrc01mafcsmdqk02dsf4523702fhk-awesome-3.5.9/share/awesome/themes/default/layouts/tileleftw.png"
-theme.layout_tile = "/nix/store/q59yrc01mafcsmdqk02dsf4523702fhk-awesome-3.5.9/share/awesome/themes/default/layouts/tilew.png"
-theme.layout_tiletop = "/nix/store/q59yrc01mafcsmdqk02dsf4523702fhk-awesome-3.5.9/share/awesome/themes/default/layouts/tiletopw.png"
-theme.layout_spiral  = "/nix/store/q59yrc01mafcsmdqk02dsf4523702fhk-awesome-3.5.9/share/awesome/themes/default/layouts/spiralw.png"
-theme.layout_dwindle = "/nix/store/q59yrc01mafcsmdqk02dsf4523702fhk-awesome-3.5.9/share/awesome/themes/default/layouts/dwindlew.png"
-
-theme.awesome_icon = "/nix/store/q59yrc01mafcsmdqk02dsf4523702fhk-awesome-3.5.9/share/awesome/icons/awesome16.png"
+theme.layout_fairh = "/home/drets/src/dots/.config/awesome/themes/default/layouts/fairhw.png"
+theme.layout_fairv = "/home/drets/src/dots/.config/awesome/themes/default/layouts/fairvw.png"
+theme.layout_floating  = "/home/drets/src/dots/.config/awesome/themes/default/layouts/floatingw.png"
+theme.layout_magnifier = "/home/drets/src/dots/.config/awesome/themes/default/layouts/magnifierw.png"
+theme.layout_max = "/home/drets/src/dots/.config/awesome/themes/default/layouts/maxw.png"
+theme.layout_fullscreen = "/home/drets/src/dots/.config/awesome/themes/default/layouts/fullscreenw.png"
+theme.layout_tilebottom = "/home/drets/src/dots/.config/awesome/themes/default/layouts/tilebottomw.png"
+theme.layout_tileleft   = "/home/drets/src/dots/.config/awesome/themes/default/layouts/tileleftw.png"
+theme.layout_tile = "/home/drets/src/dots/.config/awesome/themes/default/layouts/tilew.png"
+theme.layout_tiletop = "/home/drets/src/dots/.config/awesome/themes/default/layouts/tiletopw.png"
+theme.layout_spiral  = "/home/drets/src/dots/.config/awesome/themes/default/layouts/spiralw.png"
+theme.layout_dwindle = "/home/drets/src/dots/.config/awesome/themes/default/layouts/dwindlew.png"
 
 -- Define the icon theme for application icons. If not set then the icons 
 -- from /usr/share/icons and /usr/share/icons/hicolor will be used.
 theme.icon_theme = nil
 
 return theme
--- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
