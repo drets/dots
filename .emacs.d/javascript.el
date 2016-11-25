5b4c11318ee504cca43017acf31f14a38a62a01b
Dmytro Rets <dmitryrets@gmail.com>
2016-11-26 00:00:39 +0100

    Use eslint from node_modules.

--- .emacs.d/javascript.el
+++ .emacs.d/javascript.el
@@ -1,6 +1,17 @@
 ;; Javascript mode settings.
 (require 'js)
 
+(defun use-eslint-from-node-modules ()
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (eslint (and root
+                      (expand-file-name "node_modules/eslint/bin/eslint.js"
+                                        root))))
+    (when (and eslint (file-executable-p eslint))
+      (setq-local flycheck-javascript-eslint-executable eslint))))
+
+(add-hook 'flycheck-mode-hook #'use-eslint-from-node-modules)
 (add-hook 'js-mode-hook #'flycheck-mode)
 
 (provide 'javascript)
