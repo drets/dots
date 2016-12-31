4e0bd5a071c81766361ab16e1f42713e894799d3
Dmytro Rets <dmitryrets@gmail.com>
2016-12-31 20:44:41 +0200

    New wallpaper.

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -39,7 +39,7 @@ end
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
 beautiful.init(awful.util.get_themes_dir() .. "default/theme.lua")
-beautiful.wallpaper = "/home/drets/.background/sky-blue.png"
+beautiful.wallpaper = "/home/drets/.background/life.jpg"
 
 -- This is used later as the default terminal and editor to run.
 terminal = "urxvt"
