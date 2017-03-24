5c76f29e1fad1fce7c9ff18dfeb62fc5349ac4b9
Dmitry Rets <dmitryrets@gmail.com>
2017-03-24 23:13:55 +0200

    [emacs] simplify org-capture templates.

--- .emacs.d/org-wrapper.el
+++ .emacs.d/org-wrapper.el
@@ -1,17 +1,13 @@
 ;; Shell adjustments.
 (require #'org)
+(require 'org-drill)
 
 (setq org-capture-templates
       '(("a" "Appointment" entry (file+headline "~/org/tasks.org" "Calendar")
-         "* APPT %^{Description} %^g\n%?\nAdded: %U")
-         ("n" "Note" entry (file+datetree "~/org/tasks.org")
-         "* %^{Description} %^g\n%?\nAdded: %U")
-         ("t" "Task" entry (file+datetree "~/org/tasks.org")
-         "* TODO %^{Description}  %^g\n%?\nAdded: %U")
-         ("j" "Journal" entry (file+datetree
-         "~/org/journal.org")
-         "** %^{Description}\n%?")))
-
-(require 'org-drill)
+         "* APPT %?\nSCHEDULED: %(org-insert-time-stamp (org-read-date nil t \"+0d\"))\n\n%U")
+        ("n" "Note" entry (file+datetree "~/org/tasks.org") "* %?\n\n%U")
+        ("t" "Task" entry (file+datetree "~/org/tasks.org")
+         "* TODO %?\nSCHEDULED: %(org-insert-time-stamp (org-read-date nil t \"+0d\"))\n\n\npomodoros: 0\n%U")
+        ("j" "Journal" entry (file+datetree "~/org/journal.org") "** %?")))
 
 (provide 'org-wrapper)
