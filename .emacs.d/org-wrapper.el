4fbf5c67b07ee2981499cdf76b04a9e7157610c8
Dmitry Rets <dmitryrets@gmail.com>
2017-03-28 00:01:21 +0300

    [emacs] minor.

--- .emacs.d/org-wrapper.el
+++ .emacs.d/org-wrapper.el
@@ -7,7 +7,7 @@
          "* APPT %?\nSCHEDULED: %(org-insert-time-stamp (org-read-date nil t \"+0d\"))\n\n%U")
         ("n" "Note" entry (file+datetree "~/org/tasks.org") "* %?\n\n%U")
         ("t" "Task" entry (file+datetree "~/org/tasks.org")
-         "* TODO %?\nSCHEDULED: %(org-insert-time-stamp (org-read-date nil t \"+0d\"))\n\n\npomodoros: 0\n%U")
+         "* TODO %?\nSCHEDULED: %(org-insert-time-stamp (org-read-date nil t \"+0d\"))\n\n%U")
         ("j" "Journal" entry (file+datetree "~/org/journal.org") "** %?")))
 
 (provide 'org-wrapper)
